9bd7932741921582c81fe63213c83829cd1fc021
harryjamesuk <harryjamesuk+github@gmail.com>
2014-07-30 18:53:06 +0100

    .gitignore refresh

---
+++ app/src/main/java/com/harryjamesuk/workshopapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.harryjamesuk.workshopapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.ParseObject;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button coolButton = (Button) findViewById(R.id.button);
+        coolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, getString(R.string.special_text), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Treehouse temp = new Treehouse();
+
+        ParseObject object;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/harryjamesuk/workshopapp/Treehouse.java
@@ -0,0 +1,7 @@
+package com.harryjamesuk.workshopapp;
+
+/**
+ * Created by Harry on 25/07/2014.
+ */
+public class Treehouse {
+}
